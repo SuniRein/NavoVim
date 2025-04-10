c9c01791a7c0481375d68ebc86b8fcd3d8ea6be6
SuniRein <sunirein@qq.com>
2025-04-10 09:24:14 +0800

    add colorsheme catppuccin

---
+++ lua/plugins/colorsheme.lua
@@ -0,0 +1,14 @@
+return {
+    -- catppuccin
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        opts = {
+            flavor = "mocha",
+        },
+        init = function()
+            vim.cmd.colorscheme("catppuccin")
+        end,
+    },
+}
