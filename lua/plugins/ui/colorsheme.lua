56be78c885eb58cf9f5ca154ec38fdfe20119ddd
SuniRein <sunirein@qq.com>
2025-04-11 16:26:25 +0800

    integrate nvim-cmp with catppuccin

--- lua/plugins/ui/colorsheme.lua
+++ lua/plugins/ui/colorsheme.lua
@@ -5,7 +5,25 @@ return {
         name = "catppuccin",
         priority = 1000,
         opts = {
-            flavor = "mocha",
+            background = { light = "latte", dark = "mocha" }, -- latte, frappe, macchiato, mocha
+            highlight_overrides = {
+                ---@param cp palette
+                all = function(cp)
+                    return {
+                        -- For nvim-cmp
+                        Pmenu = { fg = cp.overlay2, bg = cp.base },
+                        PmenuBorder = { fg = cp.surface1, bg = cp.base },
+                        PmenuSel = { bg = cp.green, fg = cp.base },
+                        CmpItemAbbr = { fg = cp.overlay2 },
+                        CmpItemAbbrMatch = { fg = cp.blue, style = { "bold" } },
+                        CmpDoc = { link = "NormalFloat" },
+                        CmpDocBorder = {
+                            fg = cp.mantle,
+                            bg = cp.mantle,
+                        },
+                    }
+                end,
+            },
         },
         init = function()
             vim.cmd.colorscheme("catppuccin")
