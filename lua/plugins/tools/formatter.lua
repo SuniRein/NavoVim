4aecfb90dc1c67638b7950dab19e70858dd39e49
SuniRein <sunirein@qq.com>
2025-04-11 22:56:58 +0800

    add format commands

--- lua/plugins/tools/formatter.lua
+++ lua/plugins/tools/formatter.lua
@@ -4,15 +4,52 @@ return {
         event = { "BufWritePre" },
         cmd = { "ConformInfo" },
         keys = {
-            {
-                "<M-f>",
-                function()
-                    require("conform").format({ async = true })
-                end,
-                mode = "",
-                desc = "formatter: Format buffer",
-            },
+            { "<M-f>", "<cmd>Format<CR>", desc = "formatter: Format buffer" },
         },
+
+        init = function()
+            vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+
+            vim.api.nvim_create_user_command("Format", function(args)
+                local range = nil
+                if args.count ~= -1 then
+                    local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+                    range = {
+                        start = { args.line1, 0 },
+                        ["end"] = { args.line2, end_line:len() },
+                    }
+                end
+                require("conform").format({ async = true, lsp_format = "fallback", range = range })
+            end, { range = true })
+
+            vim.api.nvim_create_user_command("FormatDisable", function(args)
+                -- FormatDisable! will disable formatting just for this buffer
+                if args.bang then
+                    vim.notify("Autoformatting disabled (buffer)", vim.log.levels.INFO, { title = "Formatter" })
+                    vim.b.disable_autoformat = true
+                else
+                    vim.notify("Autoformatting disabled (global)", vim.log.levels.INFO, { title = "Formatter" })
+                    vim.g.disable_autoformat = true
+                end
+            end, {
+                desc = "Disable autoformat-on-save",
+                bang = true,
+            })
+
+            vim.api.nvim_create_user_command("FormatEnable", function(args)
+                -- FormatEnable! will enable formatting just for this buffer
+                if args.bang then
+                    vim.notify("Autoformatting enabled (buffer)", vim.log.levels.INFO, { title = "Formatter" })
+                    vim.b.disable_autoformat = false
+                else
+                    vim.notify("Autoformatting enabled (global)", vim.log.levels.INFO, { title = "Formatter" })
+                    vim.g.disable_autoformat = false
+                end
+            end, {
+                desc = "Enable autoformat-on-save",
+                bang = true,
+            })
+        end,
 
         ---@module "conform"
         ---@type conform.setupOpts
@@ -20,17 +57,21 @@ return {
             default_format_opts = {
                 lsp_format = "fallback",
             },
-            format_on_save = { timeout_ms = 500 },
+
+            format_on_save = function(bufnr)
+                if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+                    return false
+                end
+                return { timeout_ms = 500 }
+            end,
+
             formatters_by_ft = {
                 lua = { "stylua" },
             },
+
             formatters = {
                 stylua = { prepend_args = { "--indent-type", "Spaces" } },
             },
         },
-
-        init = function()
-            vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-        end,
     },
 }
