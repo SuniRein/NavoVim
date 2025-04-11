0c2458fbb00e4975e8ce49444fd4f96ee1636449
SuniRein <sunirein@qq.com>
2025-04-11 11:19:46 +0800

    add mason.nvim

---
+++ lua/plugins/tools/lsp.lua
@@ -0,0 +1,15 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        opts = {
+            ui = {
+                border = "single",
+                icons = {
+                    package_pending = "",
+                    package_installed = "󰄳",
+                    package_uninstalled = "󰊠",
+                },
+            },
+        },
+    },
+}
