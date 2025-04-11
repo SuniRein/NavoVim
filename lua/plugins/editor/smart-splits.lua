90b3f9e7da5700c8cc9d29242617de6f510ca37f
SuniRein <sunirein@qq.com>
2025-04-11 17:31:00 +0800

    add smart-splits.nvim

---
+++ lua/plugins/editor/smart-splits.lua
@@ -0,0 +1,19 @@
+return {
+    {
+        "mrjones2014/smart-splits.nvim",
+        lazy = true,
+        event = { "CursorHoldI", "CursorHold" },
+        opts = {},
+        keys = {
+            { "<M-h>", "<cmd>SmartResizeLeft<CR>", desc = "window: Resize left" },
+            { "<M-j>", "<cmd>SmartResizeDown<CR>", desc = "window: Resize down" },
+            { "<M-k>", "<cmd>SmartResizeUp<CR>", desc = "window: Resize up" },
+            { "<M-l>", "<cmd>SmartResizeRight<CR>", desc = "window: Resize right" },
+
+            { "<C-h>", "<cmd>SmartCursorMoveLeft<CR>", desc = "window: Move left" },
+            { "<C-j>", "<cmd>SmartCursorMoveDown<CR>", desc = "window: Move down" },
+            { "<C-k>", "<cmd>SmartCursorMoveUp<CR>", desc = "window: Move up" },
+            { "<C-l>", "<cmd>SmartCursorMoveRight<CR>", desc = "window: Move right" },
+        },
+    },
+}
