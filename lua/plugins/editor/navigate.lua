684cf1f0ced96d4644db1f249e213221e55fd359
SuniRein <sunirein@qq.com>
2025-04-11 17:40:09 +0800

    add accelerated-jk.nvim

---
+++ lua/plugins/editor/navigate.lua
@@ -0,0 +1,11 @@
+return {
+    {
+        "rainbowhxch/accelerated-jk.nvim",
+        event = "VeryLazy",
+        opts = {},
+        keys = {
+            { "j", "<Plug>(accelerated_jk_gj)", desc = "accelerated j" },
+            { "k", "<Plug>(accelerated_jk_gk)", desc = "accelerated k" },
+        },
+    },
+}
