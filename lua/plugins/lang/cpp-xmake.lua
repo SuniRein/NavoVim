e5d5cf6c99db4f9a1df09442fc838d8d24d85fbc
SuniRein <sunirein@qq.com>
2025-04-12 23:26:47 +0800

    fix: xmake.nvim may not load with multiple LspAttach events

--- lua/plugins/lang/cpp-xmake.lua
+++ lua/plugins/lang/cpp-xmake.lua
@@ -6,7 +6,6 @@ return {
         },
         init = function()
             vim.api.nvim_create_autocmd("LspAttach", {
-                once = true,
                 callback = function(args)
                     local client = vim.lsp.get_client_by_id(args.data.client_id)
                     if client and client.name == "clangd" then
