5580b7338745ac0d1aa833284ea45f77f428b129
SuniRein <sunirein@qq.com>
2025-04-15 16:14:35 +0800

    plugin: add vim-cool

---
+++ lua/plugins/ui/vim-cool.lua
@@ -0,0 +1,8 @@
+return {
+    -- disable search highlighting when done searching
+    -- re-enables it when searching again
+    {
+        "romainl/vim-cool",
+        event = "CmdlineEnter",
+    },
+}
