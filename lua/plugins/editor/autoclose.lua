cb4e559154f0dc1d0ee9f59f66a1e95348793054
SuniRein <sunirein@qq.com>
2025-04-17 08:39:44 +0800

    plugin: enhance autoclose for markdown

--- lua/plugins/editor/autoclose.lua
+++ lua/plugins/editor/autoclose.lua
@@ -15,7 +15,13 @@ return {
 
             ['"'] = { escape = true, close = true, pair = '""' },
             ["'"] = { escape = true, close = true, pair = "''", disabled_filetypes = { "rust" } },
-            ["`"] = { escape = true, close = true, pair = "``" },
+            ["`"] = { escape = false, close = true, pair = "``" },
+
+            ["$"] = { escape = false, close = true, pair = "$$", enabled_filetypes = { "markdown", "latex" } },
+        },
+
+        options = {
+            pair_spaces = true,
         },
     },
 }
