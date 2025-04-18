0671e78e1a0997ad5f8887c34d66a43d59696388
SuniRein <sunirein@qq.com>
2025-04-18 08:52:08 +0800

    fix: lazy load markview.nvim conflict with snacks.picker

--- lua/plugins/lang/markdown.lua
+++ lua/plugins/lang/markdown.lua
@@ -8,10 +8,7 @@ local supported_filetypes = {
 return {
     {
         "OXY2DEV/markview.nvim",
-        dependencies = {
-            "folke/snacks.nvim",
-        },
-        ft = { "markdown" },
+        lazy = false, -- lazy load markview conflicts with snacks.picker
         init = function()
             vim.api.nvim_create_autocmd("FileType", {
                 pattern = supported_filetypes,
@@ -19,50 +16,53 @@ return {
                     vim.opt_local.wrap = false
                 end,
             })
+
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "VeryLazy",
+                callback = function()
+                    local mv = require("markview")
+
+                    Snacks.toggle({
+                        name = "Markview",
+                        get = function()
+                            return mv.actions.__is_enabled()
+                        end,
+                        set = function(state)
+                            if state then
+                                mv.actions.enable()
+                            else
+                                mv.actions.disable()
+                            end
+                        end,
+                    }):map("<leader>um")
+
+                    Snacks.toggle({
+                        name = "Markview Split",
+                        get = function()
+                            return type(mv.state.splitview_source) == "number"
+                        end,
+                        set = function(state)
+                            if state then
+                                mv.actions.splitOpen()
+                            else
+                                mv.actions.splitClose()
+                            end
+                        end,
+                    }):map("<leader>uM")
+                end,
+            })
         end,
-        opts = function()
-            local mv = require("markview")
-
-            Snacks.toggle({
-                name = "Markview",
-                get = function()
-                    return mv.actions.__is_enabled()
-                end,
-                set = function(state)
-                    if state then
-                        mv.actions.enable()
-                    else
-                        mv.actions.disable()
-                    end
-                end,
-            }):map("<leader>um")
-
-            Snacks.toggle({
-                name = "Markview Split",
-                get = function()
-                    return type(mv.state.splitview_source) == "number"
-                end,
-                set = function(state)
-                    if state then
-                        mv.actions.splitOpen()
-                    else
-                        mv.actions.splitClose()
-                    end
-                end,
-            }):map("<leader>uM")
-
-            return {
-                preview = {
-                    filetypes = supported_filetypes,
-                    hybrid_modes = { "n" },
-                    linewise_hybrid_mode = true,
+        opts = {
+            preview = {
+                filetypes = supported_filetypes,
+                hybrid_modes = { "n" },
+                linewise_hybrid_mode = true,
+            },
+            markdown = {
+                table = {
+                    use_virt_lines = true,
                 },
-                markdown = {
-                    table = {
-                        use_virt_lines = true,
-                    },
-                },
-            }
-        end,
+            },
+        },
     },
 }
