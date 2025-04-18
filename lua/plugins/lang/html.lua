c46a75094979d8e5a34ff86bdf5c3d0d439138fa
SuniRein <sunirein@qq.com>
2025-04-18 13:24:41 +0800

    plugin: add nvim-ts-autotag

---
+++ lua/plugins/lang/html.lua
@@ -0,0 +1,20 @@
+return {
+    {
+        "windwp/nvim-ts-autotag",
+        ft = {
+            "html",
+            "xml",
+            "javascriptreact",
+            "typescriptreact",
+            "vue",
+            "markdown",
+        },
+        opts = {
+            opts = {
+                enable_close = true,
+                enable_rename = true,
+                enable_close_on_slash = true,
+            },
+        },
+    },
+}
