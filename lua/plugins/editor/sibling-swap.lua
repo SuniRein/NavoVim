04f43abc0c635749b9e8f0dbd2f9cb545b7f0956
SuniRein <sunirein@qq.com>
2025-04-21 11:30:47 +0800

    keymap: adjust swap keymap since conflict with <C-O>

--- lua/plugins/editor/sibling-swap.lua
+++ lua/plugins/editor/sibling-swap.lua
@@ -5,16 +5,16 @@ return {
             "nvim-treesitter/nvim-treesitter",
         },
         opts = {
-            highlight_node_at_cursor = true,
             keymaps = {
-                ["<C-i>"] = "swap_with_left",
-                ["<C-o>"] = "swap_with_right",
+                ["<M-[>"] = "swap_with_left",
+                ["<M-]>"] = "swap_with_right",
             },
+            highlight_node_at_cursor = true,
             interline_swaps_without_separator = true,
         },
         keys = {
-            { "<C-i>" },
-            { "<C-o>" },
+            { "<M-[>" },
+            { "<M-]>" },
         },
     },
 }
