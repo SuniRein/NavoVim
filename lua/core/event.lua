daec0b3c0932053d9db0d9870b5b15db1339f1aa
SuniRein <sunirein@qq.com>
2025-04-30 19:55:34 +0800

    check "zathura" before run "OpenPdf"

--- lua/core/event.lua
+++ lua/core/event.lua
@@ -48,6 +48,11 @@ vim.api.nvim_create_autocmd("FileType", {
     pattern = "typst",
     callback = function(param)
         vim.api.nvim_buf_create_user_command(param.buf, "OpenPdf", function()
+            if vim.fn.executable("zathura") == 0 then
+                vim.notify("Zathura not found", vim.log.levels.ERROR)
+                return
+            end
+
             local filepath = vim.api.nvim_buf_get_name(param.buf)
             if filepath:match("%.typ$") then
                 local pdf_path = filepath:gsub("%.typ$", ".pdf")
