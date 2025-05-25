54f9f6e7546148f6d293b2896d301bf68120d359
SuniRein <sunirein@qq.com>
2025-05-25 08:43:50 +0800

    feat(grug_far): more keymaps

--- lua/plugins/editor/replace.lua
+++ lua/plugins/editor/replace.lua
@@ -1,3 +1,24 @@
+local function open_grug_far(engine)
+    local grug = require("grug-far")
+    local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+    grug.open({
+        transient = true,
+        prefills = {
+            filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+        },
+        engine = engine,
+    })
+end
+
+local function open_grug_far_within(engine)
+    local grug = require("grug-far")
+    grug.open({
+        transient = true,
+        engine = engine,
+        visualSelectionUsage = "operate-within-range",
+    })
+end
+
 return {
     {
         "magicduck/grug-far.nvim",
@@ -9,17 +30,35 @@ return {
             {
                 "<leader>sR",
                 function()
-                    local grug = require("grug-far")
-                    local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
-                    grug.open({
-                        transient = true,
-                        prefills = {
-                            filesFilter = ext and ext ~= "" and "*." .. ext or nil,
-                        },
-                    })
+                    open_grug_far("ripgrep")
                 end,
-                mode = { "n", "v" },
-                desc = "Search and Replace",
+                mode = "n",
+                desc = "Search and Replace (ripgrep)",
+            },
+            {
+                "<leader>sA",
+                function()
+                    open_grug_far("astgrep")
+                end,
+                mode = "n",
+                desc = "Search and Replace (astgrep)",
+            },
+
+            {
+                "<leader>sR",
+                function()
+                    open_grug_far_within("ripgrep")
+                end,
+                mode = "v",
+                desc = "Search and Replace (ripgrep)",
+            },
+            {
+                "<leader>sA",
+                function()
+                    open_grug_far_within("astgrep")
+                end,
+                mode = "v",
+                desc = "Search and Replace (astgrep)",
             },
         },
     },
