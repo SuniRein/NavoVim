05c96c5b4e868850969226ace959aa7c169c85fc
SuniRein <sunirein@qq.com>
2025-06-16 17:37:08 +0800

    feat(dap): use dap.utils.pick_file

--- lua/plugins/tools/dap.lua
+++ lua/plugins/tools/dap.lua
@@ -3,6 +3,7 @@ return {
         "mfussenegger/nvim-dap",
         config = function()
             local dap = require("dap")
+            local utils = require("dap.utils")
 
             dap.adapters.codelldb = {
                 type = "executable",
@@ -15,7 +16,7 @@ return {
                     type = "codelldb",
                     request = "launch",
                     program = function()
-                        return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+                        return utils.pick_file()
                     end,
                     cwd = "${workspaceFolder}",
                     stopOnEntry = false,
