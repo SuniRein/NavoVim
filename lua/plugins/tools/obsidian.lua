4e95ba17822d89eb6b0141bb8326cf9acd72115c
SuniRein <sunirein@qq.com>
2025-07-01 14:53:00 +0800

    feat(obsidian): adjust formatter

--- lua/plugins/tools/obsidian.lua
+++ lua/plugins/tools/obsidian.lua
@@ -10,7 +10,6 @@ return {
         },
         ft = "markdown",
         ---@module "obsidian"
-        ---@type obsidian.config
         opts = function()
             local keymaps = {
                 oo = { "quick_switch", "Search by file name" },
@@ -50,6 +49,16 @@ return {
                         end
                     end
                     return suffix
+                end,
+                note_frontmatter_func = function(note)
+                    local out = { aliases = note.aliases, tags = note.tags }
+                    if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
+                        for k, v in pairs(note.metadata) do
+                            out[k] = v
+                        end
+                    end
+
+                    return out
                 end,
                 completion = {
                     nvim_cmp = false,
