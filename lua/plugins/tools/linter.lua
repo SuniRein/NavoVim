ebb83c5400d6194682edd5862e235d3eb74a6417
SuniRein <sunirein@qq.com>
2025-07-03 11:09:31 +0800

    feat(linter): only enabled eslint_d when needed

--- lua/plugins/tools/linter.lua
+++ lua/plugins/tools/linter.lua
@@ -4,15 +4,33 @@ return {
     config = function()
         local lint = require("lint")
 
-        vim.env.ESLINT_D_PPID = vim.fn.getpid()
         lint.linters_by_ft = {
-            javascript = { "eslint_d" },
-            typescript = { "eslint_d" },
-            javascriptreact = { "eslint_d" },
-            typescriptreact = { "eslint_d" },
-            vue = { "eslint_d" },
             ghaction = { "actionlint" },
         }
+
+        local js_filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" }
+        local eslint_markers = {
+            ".eslintrc",
+            ".eslintrc.js",
+            ".eslintrc.cjs",
+            ".eslintrc.yaml",
+            ".eslintrc.yml",
+            ".eslintrc.json",
+            "eslint.config.js",
+            "eslint.config.mjs",
+            "eslint.config.cjs",
+            "eslint.config.ts",
+            "eslint.config.mts",
+            "eslint.config.cts",
+        }
+
+        local cwd = vim.fn.getcwd()
+        if require("lspconfig.util").root_pattern(eslint_markers)(cwd) then
+            vim.env.ESLINT_D_PPID = vim.fn.getpid()
+            for _, ft in ipairs(js_filetypes) do
+                lint.linters_by_ft[ft] = { "eslint" }
+            end
+        end
 
         vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave", "TextChanged" }, {
             callback = function()
