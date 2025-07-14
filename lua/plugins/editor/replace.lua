b1afcaf6740c4b7406e4bef606ead3372dd6d111
SuniRein <sunirein@qq.com>
2025-07-14 16:02:49 +0800

    feat(plugin): add nvim-rip-substitute

--- lua/plugins/editor/replace.lua
+++ lua/plugins/editor/replace.lua
@@ -62,4 +62,20 @@ return {
             },
         },
     },
+
+    {
+        "chrisgrieser/nvim-rip-substitute",
+        cmd = "RipSubstitute",
+        opts = {},
+        keys = {
+            {
+                "g/",
+                function()
+                    require("rip-substitute").sub()
+                end,
+                mode = { "n", "x" },
+                desc = "Rip Substitute",
+            },
+        },
+    },
 }
