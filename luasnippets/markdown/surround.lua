c92f09f3308c0a5d1a78559fff1c7de777e5bb43
SuniRein <sunirein@qq.com>
2025-08-17 14:06:16 +0800

    snippets(markdown): add surround snippets

---
+++ luasnippets/markdown/surround.lua
@@ -0,0 +1,34 @@
+local utils = require("utils.luasnip")
+
+local function surround(trigger, dscr, text)
+    return s(
+        {
+            trig = trigger,
+            dscr = dscr,
+            snippetType = "autosnippet",
+        },
+        fmta(text .. "<>" .. text, {
+            d(1, utils.get_visual),
+        })
+    )
+end
+
+return {
+    surround(";b", "Bold text", "**"),
+    surround(";i", "Italic text", "*"),
+    surround(";I", "Blod Italic text", "***"),
+    surround(";h", "Highlight block", "=="),
+    surround(";c", "Code block", "`"),
+    surround(";m", "Math block", "$"),
+    s(
+        {
+            trig = ";l",
+            dscr = "Link",
+            snippetType = "autosnippet",
+        },
+        fmta("[<>](<>)", {
+            d(1, utils.get_visual),
+            i(2, "https://example.com"),
+        })
+    ),
+}
