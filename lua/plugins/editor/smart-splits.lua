6bb0d3fbd7659d3799a94050af850f0f6d73ba16
SuniRein <sunirein@qq.com>
2025-09-12 12:57:08 +0800

    feat(smartsplit): update keymaps

--- lua/plugins/editor/smart-splits.lua
+++ lua/plugins/editor/smart-splits.lua
@@ -8,15 +8,15 @@ return {
             local ss = require("smart-splits")
             local map = vim.keymap.set
 
-            map("n", "<M-h>", ss.resize_left, { desc = "Window resize left" })
-            map("n", "<M-j>", ss.resize_down, { desc = "Window resize down" })
-            map("n", "<M-k>", ss.resize_up, { desc = "Window resize up" })
-            map("n", "<M-l>", ss.resize_right, { desc = "Window resize right" })
+            map("n", "<M-S-h>", ss.resize_left, { desc = "Window resize left" })
+            map("n", "<M-S-j>", ss.resize_down, { desc = "Window resize down" })
+            map("n", "<M-S-k>", ss.resize_up, { desc = "Window resize up" })
+            map("n", "<M-S-l>", ss.resize_right, { desc = "Window resize right" })
 
-            map("n", "<C-h>", ss.move_cursor_left, { desc = "Window move left" })
-            map("n", "<C-j>", ss.move_cursor_down, { desc = "Window move down" })
-            map("n", "<C-k>", ss.move_cursor_up, { desc = "Window move up" })
-            map("n", "<C-l>", ss.move_cursor_right, { desc = "Window move right" })
+            map("n", "<C-S-h>", ss.move_cursor_left, { desc = "Window move left" })
+            map("n", "<C-S-j>", ss.move_cursor_down, { desc = "Window move down" })
+            map("n", "<C-S-k>", ss.move_cursor_up, { desc = "Window move up" })
+            map("n", "<C-S-l>", ss.move_cursor_right, { desc = "Window move right" })
         end,
     },
 }
