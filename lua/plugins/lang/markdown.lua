4a05d12bbb9fc633ec2a5e7c46f2cf47593faf50
SuniRein <sunirein@qq.com>
2025-09-28 10:25:24 +0800

    feat(plugin): add markdown-preview.nvim

--- lua/plugins/lang/markdown.lua
+++ lua/plugins/lang/markdown.lua
@@ -66,4 +66,14 @@ return {
             },
         },
     },
+
+    {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function()
+            require("lazy").load({ plugins = { "markdown-preview.nvim" } })
+            vim.fn["mkdp#util#install"]()
+        end,
+    },
 }
