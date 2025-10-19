09d1296696a941d16569b9747e5ed7fd6a0fc90a
SuniRein <sunirein@qq.com>
2025-10-19 11:13:59 +0800

    feat(snippet/markdown): add block level codeblock

--- luasnippets/markdown/surround.lua
+++ luasnippets/markdown/surround.lua
@@ -18,7 +18,7 @@ return {
     surround(";i", "Italic text", "*"),
     surround(";I", "Blod Italic text", "***"),
     surround(";h", "Highlight block", "=="),
-    surround(";c", "Code block", "`"),
+    surround(";c", "Code block (inline)", "`"),
     surround(";m", "Math block", "$"),
     s(
         {
@@ -30,5 +30,23 @@ return {
             d(1, utils.get_visual),
             i(2, "https://example.com"),
         })
+    ),
+    s(
+        {
+            trig = ";C",
+            dscr = "Code block (block)",
+            snippetType = "autosnippet",
+        },
+        fmt(
+            [[
+                ```{}
+                {}
+                ```
+            ]],
+            {
+                i(1, "lang"),
+                d(2, utils.get_visual),
+            }
+        )
     ),
 }
