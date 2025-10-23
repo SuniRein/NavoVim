37cdcd869fcce41a49975fad6b9197e43b3fbc92
SuniRein <sunirein@qq.com>
2025-10-23 10:16:21 +0800

    feat(whichkey): add icon for group

--- lua/plugins/keymap.lua
+++ lua/plugins/keymap.lua
@@ -4,19 +4,23 @@ return {
         dependencies = { "nvim-tree/nvim-web-devicons" },
         event = "VeryLazy",
         opts_extend = { "spec" },
+        ---@module "which-key"
+        ---@type wk.Config
+        ---@diagnostic disable: missing-fields
         opts = {
             preset = "modern",
             spec = {
                 { "<leader>b", group = "Buffer" },
-                { "<leader>c", group = "Current" },
+                { "<leader>c", group = "Current", icon = " " },
                 { "<leader>f", group = "Find" },
                 { "<leader>g", group = "Git" },
                 { "<leader>n", group = "Notification" },
                 { "<leader>s", group = "Search" },
-                { "<leader>t", group = "Test" },
+                { "<leader>t", group = "Test", icon = " " },
                 { "<leader>u", group = "UI" },
-                { "<leader>l", group = "Trouble" },
-                { "<leader>i", group = "Lsp" },
+                { "<leader>l", group = "Trouble", icon = " " },
+                { "<leader>i", group = "Lsp", icon = { icon = " ", color = "cyan" } },
+                { "<leader>w", group = "Wtf", icon = { icon = "󱄶 ", color = "red" } },
 
                 { "gs", group = "Surround" },
                 { "ga", group = "Change text case" },
