15f09dd8d0aa7dfe6a5097e7348aeb73eed127de
SuniRein <sunirein@qq.com>
2025-11-09 17:44:53 +0800

    feat: add tab option for Indent command

--- lua/core/commands.lua
+++ lua/core/commands.lua
@@ -1,4 +1,10 @@
 vim.api.nvim_create_user_command("Indent", function(opts)
+    if opts.args == "tab" then
+        vim.opt_local.expandtab = false
+        print("Indentation set to tabs")
+        return
+    end
+
     local indent_size = tonumber(opts.args)
     vim.opt_local.shiftwidth = indent_size
     vim.opt_local.tabstop = indent_size
