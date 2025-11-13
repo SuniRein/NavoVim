7afb31e8746510d4bce8a94b18f94ee99e56e4bb
SuniRein <sunirein@qq.com>
2025-11-13 11:09:56 +0800

    feat(ai): add opencode.nvim

--- lua/plugins/tools/ai.lua
+++ lua/plugins/tools/ai.lua
@@ -60,6 +60,44 @@ return {
     },
 
     {
+        "NickvanDyke/opencode.nvim",
+        dependencies = {
+            { "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
+        },
+        config = function()
+            ---@type opencode.Opts
+            vim.g.opencode_opts = {}
+
+            vim.o.autoread = true
+
+            vim.keymap.set(
+                { "n", "x" },
+                "<leader>oa",
+                function() require("opencode").ask("@this: ", { submit = true }) end,
+                { desc = "Ask opencode" }
+            )
+            vim.keymap.set(
+                { "n", "x" },
+                "<leader>ox",
+                function() require("opencode").select() end,
+                { desc = "Execute opencode action…" }
+            )
+            vim.keymap.set(
+                { "n", "x" },
+                "<leader>oA",
+                function() require("opencode").prompt("@this") end,
+                { desc = "Add to opencode" }
+            )
+            vim.keymap.set(
+                { "n", "t" },
+                "<M-.>",
+                function() require("opencode").toggle() end,
+                { desc = "Toggle opencode" }
+            )
+        end,
+    },
+
+    {
         "zbirenbaum/copilot.lua",
         cmd = "Copilot",
         event = "InsertEnter",
