211a9555b2f1cbffe5808bcc2ba75ea8f2346e42
SuniRein <sunirein@qq.com>
2025-12-18 19:37:52 +0800

    feat(treesitter/rust): inject sqlx macro

---
+++ after/queries/rust/injections.scm
@@ -0,0 +1,28 @@
+;; extends
+
+; Rust SQLx query macro injections
+(
+  (macro_invocation
+    (scoped_identifier
+      path: (identifier) @_path
+      name: (identifier) @_identifier)
+
+    (token_tree (raw_string_literal (string_content) @injection.content)))
+
+  (#eq? @_path "sqlx")
+  (#match? @_identifier "^query")
+  (#set! injection.language "sql")
+)
+
+(
+  (macro_invocation
+    (scoped_identifier
+      path: (identifier) @_path
+      name: (identifier) @_identifier)
+
+    (token_tree (string_literal (string_content) @injection.content) ))
+
+  (#eq? @_path "sqlx")
+  (#match? @_identifier "^query")
+  (#set! injection.language "sql")
+)
